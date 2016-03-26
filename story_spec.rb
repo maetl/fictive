77ee78916ba96743add1a8d0ac723cac34cdea8e
Mark Rickerby <me@maetl.net>
2016-03-27 02:41:09 +1100

    Basic story sequence generator

---
+++ story_spec.rb
@@ -0,0 +1,24 @@
+require './story'
+
+describe Story do
+  let(:story) do
+    Story.demo
+  end
+
+  specify '#has_next?' do
+    expect(story.has_next?).to be true
+  end
+
+  specify '#next' do
+    scene = story.next
+    expect(scene.path).to eq('hello')
+    expect(scene.choices.count).to eq(1)
+  end
+
+  specify '#choose_path' do
+    path = story.next.choices.first.path
+    story.choose_path(path)
+    expect(story.next.path).to eq('goodbye')
+    expect(story.has_next?).to be false
+  end
+end
