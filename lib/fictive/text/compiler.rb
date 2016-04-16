11f160dd63cf3d21c1c23f73097562ca1c3d7ede
Mark Rickerby <me@maetl.net>
2016-04-16 20:10:03 +1000

    Add experimental conditional syntax to text compiler
    This is a first attempt, just to get *something* working. The parsing is
    messy and unfinished, but should provide enough of a starting point to
    explore different variations of template syntax and decide which one
    works best in the context of prose text. This feature isn’t currently
    being used in Muturangi, so it’s a bit more speculative for now (though
    there are a few places where I really want to use this capability).

--- lib/fictive/text/compiler.rb
+++ lib/fictive/text/compiler.rb
@@ -1,8 +1,68 @@
+require 'strscan'
+
 module Fictive
   module Text
     class Compiler
-      def initialize(source)
-        @source = source
+      def initialize(input)
+        @input = input
+      end
+
+      def process
+        @scanner = StringScanner.new(@input)
+
+        Fictive::Text::Node.new(*parse_fragment)
+      end
+
+      private
+
+      attr_reader :scanner
+
+      def parse_fragment
+        concat_fragment = scanner.scan_until(/{/)
+
+        if concat_fragment
+          [Fictive::Text::TextNode.new(concat_fragment.gsub(/{/, '')), parse_delimiter]
+        else
+          fragment = scanner.scan(/.+/)
+          Fictive::Text::TextNode.new(fragment)
+        end
+      end
+
+      def parse_delimiter
+        if scanner.scan(/{/)
+          parse_tag
+        else
+          parse_substitution
+        end
+      end
+
+      def parse_tag
+        scanner.skip(/\s+/)
+
+        parse_conditional if scanner.scan(/if/)
+      end
+
+      def parse_conditional
+        raise 'missing whitespace in conditional tag' unless scanner.scan(/\s+/)
+
+        expression = parse_expression
+        scanner.skip(/}}/)
+        consequent = scanner.scan_until(/{{\/if}}/).gsub(/{{\/if}}/, '')
+
+        Fictive::Text::ConditionalNode.new(
+          Fictive::Text::ExpressionNode.new(
+            expression
+          ),
+          Fictive::Text::TextNode.new(consequent)
+        )
+      end
+
+      def parse_expression
+        Fictive::Text::TrueNode.new if scanner.scan(/true/)
+      end
+
+      def parse_substitution
+
       end
     end
 
--- spec/text/compiler_spec.rb
+++ spec/text/compiler_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe Fictive::Text::Compiler do
+
+  describe '#process' do
+    it 'should build node from literal text' do
+      node = Fictive::Text::Compiler.new("literal text").process
+      expect(node.evaluate).to eq("literal text")
+    end
+
+    it 'should build conditional node from expression' do
+      node = Fictive::Text::Compiler.new("{{if true}}hello{{/if}}").process
+      expect(node.evaluate).to eq("hello")
+    end
+  end
+
   specify Fictive::Text::TextNode do
     expr = Fictive::Text::TextNode.new("literal")
     expect(expr.evaluate).to eq("literal")
