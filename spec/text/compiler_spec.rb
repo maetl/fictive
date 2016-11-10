381190d36801fb6c53fb501215de4bab2030c385
Mark Rickerby <me@maetl.net>
2016-11-10 23:11:37 +1100

    Update compiler spec to support new syntax ideas

--- spec/text/compiler_spec.rb
+++ spec/text/compiler_spec.rb
@@ -19,23 +19,29 @@ describe Fictive::Text::Compiler do
     end
 
     it 'should evaluate multiple text substitutions with concatenation' do
-      compiler = Fictive::Text::Compiler.new("{hello} {world}.")
+      compiler = Fictive::Text::Compiler.new("{ hello } {world}.")
       node = compiler.process(hello: "Hello", world: "World")
       expect(node.evaluate).to eq("Hello World.")
     end
 
     it 'should evaluate conditional node from directive' do
-      node = Fictive::Text::Compiler.new("/if true: hello/").process
+      node = Fictive::Text::Compiler.new("[if true: hello]").process
       expect(node.evaluate).to eq("hello")
     end
 
     it 'should concatenate conditional node from directive' do
-      node = Fictive::Text::Compiler.new("Colorless /if true: green ideas./").process
+      node = Fictive::Text::Compiler.new("Colorless [if true: green ideas.]").process
       expect(node.evaluate).to eq("Colorless green ideas.")
     end
 
     it 'should concatenate head and tails around conditional node from directive' do
-      node = Fictive::Text::Compiler.new("Colorless /if true: green ideas/ sleep furiously.").process
+      node = Fictive::Text::Compiler.new("Colorless [if true: green ideas] sleep furiously.").process
+      expect(node.evaluate).to eq("Colorless green ideas sleep furiously.")
+    end
+
+    xit 'should evaluate a substitution inside a conditional directive' do
+      compiler = Fictive::Text::Compiler.new("Colorless [if true: {colored} ideas] sleep furiously.")
+      node = compiler.process(colored: "green")
       expect(node.evaluate).to eq("Colorless green ideas sleep furiously.")
     end
   end
