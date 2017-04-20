36eaba72d519a8dc34a817789c78344c41dff93e
Mark Rickerby <me@maetl.net>
2017-04-20 22:02:42 +1000

    Rename test cases to spec identifiers
    A bit easier to read and work with than using integers for labelling.

--- spec/bundle_spec.rb
+++ spec/bundle_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Fictive::Bundle do
   describe '#import' do
     it 'imports a bundle from a single file' do
-      bundle = Fictive::Bundle.import('./spec/cases/1.txt')
+      bundle = Fictive::Bundle.import('./spec/cases/id_heading.txt')
 
       expect(bundle).to be_a(Fictive::Bundle)
       expect(bundle.documents.count).to eq(1)
@@ -20,15 +20,13 @@ describe Fictive::Bundle do
   describe '#new' do
     it 'constructs a bundle from a list of files' do
       bundle = Fictive::Bundle.new([
-        Pathname.new('./spec/cases/1.txt'),
-        Pathname.new('./spec/cases/2.txt')
+        Pathname.new('./spec/cases/id_heading.txt'),
+        Pathname.new('./spec/cases/list_items.txt')
       ])
 
       expect(bundle).to be_a(Fictive::Bundle)
       expect(bundle.documents.count).to eq(2)
       expect(bundle.passages.count).to eq(2)
-
-      p bundle.passages
     end
   end
 end
--- spec/syntax_spec.rb
+++ spec/syntax_spec.rb
@@ -6,30 +6,30 @@ describe Fictive::Syntax do
       Fictive::Syntax::Parser.new(File.read("./spec/cases/#{case_id}.txt"))
     end
 
-    it 'parses single paragraph' do
-      parser = parser_test_case(1)
+    specify 'single_paragraph' do |spec|
+      parser = parser_test_case(spec.description)
       expect(parser.parse.children.first.type).to eq(:paragraph)
       expect(parser.parse.children.first.text).to eq('Hello world.')
     end
 
-    it 'parses multiple paragraphs' do
-      parser = parser_test_case(2)
+    specify 'multiple_paragraphs' do |spec|
+      parser = parser_test_case(spec.description)
       expect(parser.parse.children.first.type).to eq(:paragraph)
       expect(parser.parse.children.first.text).to eq('Hello world.')
       expect(parser.parse.children.last.type).to eq(:paragraph)
       expect(parser.parse.children.last.text).to eq('Goodbye world.')
     end
 
-    it 'parses an id heading' do
-      parser = parser_test_case(3)
+    specify 'id_heading' do |spec|
+      parser = parser_test_case(spec.description)
       expect(parser.parse.children.first.type).to eq(:id)
       expect(parser.parse.children.first.text).to eq('hello')
       expect(parser.parse.children.last.type).to eq(:paragraph)
       expect(parser.parse.children.last.text).to eq('Hello world.')
     end
 
-    it 'parses multiple heading levels' do
-      parser = parser_test_case(4)
+    specify 'multiple_heading_levels' do |spec|
+      parser = parser_test_case(spec.description)
       expect(parser.parse.children[0].type).to eq(:h1)
       expect(parser.parse.children[0].text).to eq('Heading 1')
       expect(parser.parse.children[2].type).to eq(:h2)
@@ -38,8 +38,8 @@ describe Fictive::Syntax do
       expect(parser.parse.children[4].text).to eq('Heading 3')
     end
 
-    it 'parses list items' do
-      parser = parser_test_case(5)
+    specify 'list_items' do |spec|
+      parser = parser_test_case(spec.description)
       expect(parser.parse.children[0].type).to eq(:paragraph)
       expect(parser.parse.children[0].text).to eq('This is a paragraph.')
       expect(parser.parse.children[2].type).to eq(:list_item)
@@ -48,8 +48,8 @@ describe Fictive::Syntax do
       expect(parser.parse.children[3].text).to eq('This is also a list item')
     end
 
-    it 'parses paragraph content over multiple lines' do
-      parser = parser_test_case(6)
+    specify 'multiline_paragraph' do |spec|
+      parser = parser_test_case(spec.description)
       expect(parser.parse.children.first.type).to eq(:paragraph)
       expect(parser.parse.children.first.text).to eq('This is a paragraph which breaks over multiple lines.')
       expect(parser.parse.children.first.type).to eq(:paragraph)
