0deecf452b58a949df29096d607ed31caddbc474
Mark Rickerby <me@maetl.net>
2017-04-22 15:46:56 +1000

    Use separate fixture files in bundle spec
    Much easier to create a new set of files specifically for this test,
    rather than reuse the existing syntax cases and have to change the
    unrelated bundle spec every time the syntax changes.

--- spec/bundle_spec.rb
+++ spec/bundle_spec.rb
@@ -3,25 +3,25 @@ require 'spec_helper'
 describe Fictive::Bundle do
   describe '#import' do
     it 'imports a bundle from a single file' do
-      bundle = Fictive::Bundle.import('./spec/cases/id_heading.txt')
+      bundle = Fictive::Bundle.import('./spec/bundle/1.txt')
 
       expect(bundle).to be_a(Fictive::Bundle)
       expect(bundle.documents.count).to eq(1)
     end
 
     it 'imports a bundle from a directory' do
-      bundle = Fictive::Bundle.import('./spec/cases/')
+      bundle = Fictive::Bundle.import('./spec/bundle/')
 
       expect(bundle).to be_a(Fictive::Bundle)
-      expect(bundle.documents.count).to eq(7)
+      expect(bundle.documents.count).to eq(3)
     end
   end
 
   describe '#new' do
     it 'constructs a bundle from a list of files' do
       bundle = Fictive::Bundle.new([
-        Pathname.new('./spec/cases/id_heading.txt'),
-        Pathname.new('./spec/cases/list_items.txt')
+        Pathname.new('./spec/bundle/1.txt'),
+        Pathname.new('./spec/bundle/2.txt')
       ])
 
       expect(bundle).to be_a(Fictive::Bundle)
